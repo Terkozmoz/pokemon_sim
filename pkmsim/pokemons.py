0dcda71a9c1658e51292864d2f05881978448860
Terkozmoz <126075332+Terkozmoz@users.noreply.github.com>
2023-11-21 20:59:39 +0100

    Update pokemons.py

--- pkmsim/pokemons.py
+++ pkmsim/pokemons.py
@@ -3,7 +3,7 @@ import pokemon_Att_Repertory as att
 import random
 import pygame
 class Pokemon:
-    def __init__(self, name, max_hp, Att, speed,Def, Type):
+    def __init__(self, name, max_hp, Att, speed,Def, Type, Type2=None):
         """
         Init a Pokemon with its name, hp, attack, defense, speed and type.
 
@@ -21,17 +21,28 @@ class Pokemon:
         self.defense = Def
         self.speed = speed
         self.type = Type
+        self.type2 = Type2
         self.hp = self.calculate_hp()
         self.max_hp = self.hp
         self.attacks = self.random_attacks()
         self.status = None
+        self.attack_pp = [0, 0, 0, 0]
+        for i in range(len(self.attacks)):
+            self.attack_pp[i] = self.attacks[i].pp
 
     def random_attacks(self):
-        # Select 4 random attacks from the list of attacks available for this type of Pokemon
-        attacks_disponibles = attacks_by_type.get(self.type, [])
-        if len(attacks_disponibles) < 4:
-            raise Exception("Not enough attacks available for this type of Pokemon")
-        return random.sample(attacks_disponibles, 4)
+        if self.type2 == None:
+            # Select 4 random attacks from the list of attacks available for this type of Pokemon
+            attacks_disponibles = attacks_by_type.get(self.type, [])
+            if len(attacks_disponibles) < 4:
+                raise Exception("Not enough attacks available for this type of Pokemon")
+            return random.sample(attacks_disponibles, 4)
+        else:
+            # Select 4 random attacks from the list of attacks available for this type of Pokemon
+            attacks_disponibles = attacks_by_type.get(self.type, []) + attacks_by_type.get(self.type2, [])
+            if len(attacks_disponibles) < 4:
+                raise Exception("Not enough attacks available for this type of Pokemon")
+            return random.sample(attacks_disponibles, 4)
 
     def calculate_hp(self):
         if self.name != 'Munja':
@@ -60,19 +71,22 @@ class Pokemon:
     }
         
     def Learn_attack(self, attack):
-        if len(self.attacks) < 4 and attack not in self.attacks or attack == 'Struggle':
+        if len(self.attacks) < 4 and attack not in self.attacks:
             self.attacks.append(attack)
             print(f"{self.name} has learned {attack.name}!")
         else:
             print(f"{self.name} can't learn {attack.name}!")
 
-    def zero_pp(self):
+    def Learn_struggle(self):
+        self.attacks.append(att.Struggle)
+        self.attack_pp.append(-1)
+
+    def Zero_pp(self):
         for attack in self.attacks:
-            if attack.pp > 0:
+            if self.attack_pp[self.attacks.index(attack)] != 0:
                 return False
-            else:
-                self.Learn_attack(att.Struggle)
-                return True
+        self.Learn_struggle()
+        return True
     
     def Is_alive(self):
         """
@@ -104,9 +118,10 @@ class Pokemon:
         return self.hp
     
     def Choose_attack(self):
-        attack = random.choice(self.attacks)
-        if attack.pp == 0:
-            self.zero_pp()
+        i = random.randint(0, 3)
+        attack = self.attacks[i]
+        if self.attack_pp[i] == 0:
+            self.Zero_pp()
             return self.Choose_attack()
         return attack
 
@@ -122,7 +137,7 @@ class Pokemon:
         print(f"{self.name} a \x1b[34m{self.hp} hp\x1b[0m")
         return self.hp
 
-    def Typing(self, target) -> int:
+    def Typing(self, attack, target) -> int:
         """
         Calculating the effectiveness of an attack.
 
@@ -132,14 +147,20 @@ class Pokemon:
         Returns:
             int: The effectiveness of the attack.
         """
-        if self.type in self.TYPE_EFFECTIVENESS:
-            if target.type in self.TYPE_EFFECTIVENESS[self.type]:
-                return self.TYPE_EFFECTIVENESS[self.type][target.type]
-        return 1
+        b = 1
+        a = 1
+        if attack.attack_type in self.TYPE_EFFECTIVENESS:
+            if target.type in self.TYPE_EFFECTIVENESS[attack.attack_type]:
+                a = self.TYPE_EFFECTIVENESS[attack.attack_type][target.type]
+            if target.type2 in self.TYPE_EFFECTIVENESS:
+                if target.type2 in self.TYPE_EFFECTIVENESS[attack.attack_type]:
+                    b = self.TYPE_EFFECTIVENESS[attack.attack_type][target.type2]
+        return a * b
                 
     def Attack(self, target, attack):
         global place
         global pokemon_order
+        self.staring_hp = self.hp
         if self.status == "protect":
             self.protect = False
             print(f"{self.name}'s protection has disappeared!")
@@ -161,9 +182,9 @@ class Pokemon:
                         print(f"{self.name} can't attack!")
                         return
                 print(f"{self.name} attacks {target.name} with {attack.name}!")
-                attack.pp -= 1
+                self.attack_pp[self.attacks.index(attack)] -= 1
                 if random.randint(0,100) <= attack.accuracy:
-                    efficiency = self.Typing(target)
+                    efficiency = self.Typing(attack, target)
                     degats = attack.Calculate_damage(self, target, efficiency)
 
                     if target.status == "protect":
@@ -198,7 +219,6 @@ class Pokemon:
 
                                 if attack.effect == "paralyse":
                                     target.status = "paralyzed"
-                                    #print "a" in yellow
                                     print(f"\x1b[33m{target.name} is paralyzed!\x1b[0m")
                                 
                                 elif attack.effect == "burn":
@@ -309,7 +329,7 @@ class Pokemon:
             self.hp -= self.max_hp // 16
             print(f"{self.name} is cursed! It loses \x1b[31m{self.max_hp // 16} HP\x1b[0m.")
 
-        if self.status:
+        if self.hp != self.staring_hp:
         
             if self.hp <= 0:
                 print(f"\x1b[31m{self.name} is knocked out!\x1b[0m")
@@ -361,7 +381,7 @@ def play_music(i=0):
     elif choix == "n":
         print("You're no fun...")
     elif choix == "yes":
-        print("You found a secret music!")
+        print("You found a secret music! (yeah, just because you said yes instead of y)")
         pygame.mixer.music.load("assets\\theme\\battle_theme_alt.mp3")
         play_music(i+1)
     elif i == 10:
@@ -414,48 +434,49 @@ Squirtle = Pokemon('Squirtle', 50, 25, 43, 45, 'water')
 Charmander = Pokemon('Charmander', 65, 25, 65, 43, 'fire')
 Bulbasaur = Pokemon('Bulbasaur', 50, 31, 45, 40, 'grass')
 Charmur = Pokemon('Charmur', 60, 19, 64, 45, 'normal')
-Seel = Pokemon('Seel', 70, 28, 25, 40, 'ice')
+Seel = Pokemon('Seel', 70, 28, 25, 40, 'water')
 Meditite = Pokemon('Meditite', 60, 24, 60, 30, 'fighting')
 Gloom = Pokemon('Gloom', 85, 15, 65, 65, 'poison')
 Dugtrio = Pokemon('Dugtrio', 60, 25, 35, 35, 'ground')
 Pidgey = Pokemon('Pidgey', 70, 15, 50, 40, 'flying')
 Abra = Pokemon('Abra', 45, 39, 40, 35, 'psychic')
 Wurmple = Pokemon('Wurmple', 1, 50, 56, 30, 'insect')
-Relicanth = Pokemon('Relicanth', 65, 25, 55, 35, 'rock')
+Relicanth = Pokemon('Relicanth', 65, 25, 55, 35, 'rock', 'water')
 Shuppet = Pokemon('Shuppet', 40, 25, 50, 35, 'ghost')
 Bagon = Pokemon('Bagon', 65, 24, 50, 45, 'dragon')
 Poochyena = Pokemon('Poochyena', 30, 28, 35, 30, 'dark')
 Aron = Pokemon('Aron', 20, 23, 30, 35, 'steel')
-Marill = Pokemon('Marill', 30, 30, 25, 20, 'fairy')
+Marill = Pokemon('Marill', 30, 30, 25, 20, 'fairy','normal')
 Groudon = Pokemon('Groudon', 70, 40, 90, 90, 'ground')
 Raichu = Pokemon('Raichu', 60, 40, 110, 35, 'electric')
 Blastoise = Pokemon('Blastoise', 75, 40, 78, 100, 'water')
-Charizard = Pokemon('Charizard', 78, 40, 100, 85, 'fire')
-Venusaur = Pokemon('Venusaur', 80, 35, 70, 83, 'grass')
+Charizard = Pokemon('Charizard', 78, 40, 100, 85, 'fire', 'flying')
+Venusaur = Pokemon('Venusaur', 80, 35, 70, 83, 'grass', 'poison')
 Snorlax = Pokemon('Snorlax', 160, 50, 30, 65, 'normal')
-Lapras = Pokemon('Lapras', 130, 45, 60, 70, 'ice')
+Lapras = Pokemon('Lapras', 130, 45, 60, 70, 'ice', 'water')
 Machamp = Pokemon('Machamp', 90, 80, 45, 65, 'fighting')
-Nidoking = Pokemon('Nidoking', 81, 47, 85, 77, 'poison')
-Rhydon = Pokemon('Rhydon', 80, 45, 40, 95, 'ground')
-Aerodactyl = Pokemon('Aerodactyl', 80, 61, 110, 45, 'flying')
+Nidoking = Pokemon('Nidoking', 81, 47, 85, 77, 'poison', 'ground')
+Rhydon = Pokemon('Rhydon', 80, 45, 40, 95, 'ground', 'rock')
+Aerodactyl = Pokemon('Aerodactyl', 80, 61, 110, 45, 'flying', 'rock')
 Alakazam = Pokemon('Alakazam', 55, 50, 120, 45, 'psychic')
 Samurott = Pokemon('Samurott', 70, 85, 60, 100, 'water')
-Tyranitar = Pokemon('Tyranitar', 100, 70, 61, 110, 'rock')
+Tyranitar = Pokemon('Tyranitar', 100, 70, 61, 110, 'rock', 'dark')
 Ditto = Pokemon('Ditto', 48, 48, 48, 48, 'normal')
 Slowbro = Pokemon('Slowbro', 95, 45, 30, 55, 'psychic')
-Blissey = Pokemon('Blissey', 250, 10, 55, 10, 'fairy')
-Rayquaza = Pokemon('Rayquaza', 105, 70, 95, 90, 'dragon')
+Blissey = Pokemon('Blissey', 250, 10, 55, 10, 'fairy', 'normal')
+Rayquaza = Pokemon('Rayquaza', 105, 70, 95, 90, 'dragon', 'flying')
 Umbreon = Pokemon('Umbreon', 95, 65, 65, 110, 'dark')
 Registeel = Pokemon('Registeel', 80, 75, 50, 150, 'steel')
 Magikarp = Pokemon('Magikarp', 20, 10, 80, 55, 'water')
 Vaporeon = Pokemon('Vaporeon', 130, 65, 65, 60, 'water')
 Mew = Pokemon('Mew', 100, 100, 100, 100, 'psychic')
 Raikou = Pokemon('Raikou', 90, 85, 115, 75, 'electric')
-Articuno = Pokemon('Articuno', 90, 85, 85, 100, 'ice')
-Moltres = Pokemon('Moltres', 90, 85, 85, 75, 'fire')
+Articuno = Pokemon('Articuno', 90, 85, 85, 100, 'ice', 'flying')
+Moltres = Pokemon('Moltres', 90, 85, 85, 75, 'fire', 'flying')
+Zapdos = Pokemon('Zapdos', 90, 90, 100, 85, 'electric', 'flying')
 Mewthree = Pokemon('Mewthree', 500, 500, 500, 500, None)
 
 all_pokemon = [Pikachu, Squirtle, Charmander, Bulbasaur, Charmur, Seel, Meditite, Gloom, Dugtrio, Pidgey, Abra, Wurmple, Relicanth,
                Shuppet, Bagon, Poochyena, Aron, Marill, Groudon, Raichu, Blastoise, Charizard, Venusaur, Snorlax, Lapras, Machamp,
                Nidoking, Rhydon, Aerodactyl, Alakazam, Samurott, Tyranitar, Ditto, Slowbro, Blissey, Rayquaza, Umbreon, Registeel,
-               Magikarp, Vaporeon, Mew, Raikou, Articuno, Moltres]
+               Magikarp, Vaporeon, Mew, Raikou, Articuno, Moltres, Zapdos]
